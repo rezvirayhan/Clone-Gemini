eda228b5499734f348a4c44fa34c0a0517664db4
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2024-04-30 18:41:05 +0600

    fact:update ui & add function

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -11,17 +11,41 @@ const ContextPrivider = (props) => {
     const [loading, setLoading] = useState(false)
     const [resultData, setResultData] = useState('')
 
+
+    const delayParra = (index, nextWord) => {
+        setTimeout(function () {
+            setResultData(prev => prev + nextWord)
+        }, 75 * index)
+    }
+
+
     const onSent = async (prompt) => {
         setResultData("")
         setLoading(true)
         setShowResult(true)
         setRecentPrompt(input)
+        setPrevPrompts(prev => [...prev, input])
         const responses = await runChat(input)
-        setResultData(responses)
+        let responsesArray = responses.split("**")
+        let newResponse;
+        for (let i = 0; i < responsesArray.length; i++) {
+            if (i === 0 || i % 2 !== 1) {
+                newResponse += responsesArray[i]
+            }
+            else {
+                newResponse += "<b>" + responsesArray[i] + "</b>"
+            }
+        }
+        let newResponse2 = newResponse.split("*").join("</br>")
+        // setResultData(newResponse2)
+        let newResponseArray = newResponse2.split(" ")
+        for (let i = 0; i < newResponseArray.length; i++) {
+            const nextWord = newResponseArray[i]
+            delayParra(i, nextWord + " ")
+        }
         setLoading(false)
         setInput('')
     }
-
 
     const contextValue = {
         prevPrompts,
